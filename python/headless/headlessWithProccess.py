2099bcaada15e81b67c9e6132a9902efce056542
zhaozx <854850613@qq.com>
2017-12-31 17:58:10 +0800

    修复大小写

--- python/headless/headlessWithProccess.py
+++ python/headless/headlessWithProccess.py
@@ -6,20 +6,20 @@ from multiprocessing import Process, Pool
 
 chrome_options = Options()
 #不显示GUI
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 # chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-images')
+# chrome_options.add_argument('--disable-images')
 chrome_options.add_argument('--disable-plugins')
 #不加载图片
 prefs = {"profile.managed_default_content_settings.images":2}
 chrome_options.add_experimental_option("prefs",prefs)
 
-chrome_options.binary_location = r'C:\Users\zhaozhenxiang\AppData\Local\Google\Chrome\Application\chrome.exe'
+chrome_options.binary_location = r'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
 # chrome_options.binary_location = '/opt/google/chrome/chrome'
 chrome_options.add_argument('user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3') 
 #设置代理
 #chromeOptions.add_argument('--proxy-server=http://ip:port')  
-#chromeOptions.add_argument('--proxy-server=socks5://ip:port')  
+chrome_options.add_argument('--proxy-server=socks5://127.0.0.1:1080')  
 def load(url, times):
     # time.sleep(2)
     # return
@@ -30,7 +30,7 @@ def load(url, times):
         # print(opener.get_cookies())
         opener.delete_all_cookies()
         print(opener.title)
-        time.sleep(random.uniform(0, 1.5))
+        time.sleep(random.uniform(1, 1.5))
     
     opener.close()
         
@@ -43,6 +43,7 @@ if __name__ == '__main__':
         print('create process:' + str(i))
         # p = Process(target=load, args=('http://192.168.100.165:85/', 100,))
         p = Process(target=load, args=('http://google.com', 1,))        
+        
         p.daemon = True
         pList.append(p)
         p.start()
